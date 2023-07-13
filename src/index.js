1c85b0e790709e97261bb1836c07b541f895c86f
hc_zhang <hc_zhang@trip.com>
2023-07-13 19:40:17 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,210 +3,19 @@ import { createRoot } from "react-dom/client";
 
 import page0 from './sketch-002/pages/638A13B9-4B22-47E2-BDBA-C94201BDEF82'
 
-import { Leafer, Rect, Text, Path, Frame } from 'leafer-ui'
+import { Leafer, Rect, Text, Path, Frame, PointerEvent } from 'leafer-ui'
 
-const renderLayer = (layer, leafer) => {
-
-    let zz = null;
-
-    switch (layer._class) {
-        case 'text':
-            zz = render_text(layer, leafer);
-            break;
-        case 'rectangle':
-            zz = render_rectangle(layer, leafer);
-            break;
-        case 'shapePath':
-            zz = render_shapePath(layer, leafer);
-            break;
-        case 'symbolMaster':
-            zz = render_symbolMaster(layer, leafer);
-            break;
-        case 'symbolInstance':
-            zz = render_symbolInstance(layer, leafer);
-            break;
-        case 'bitmap':
-            zz = render_bitmap(layer, leafer);
-            break;
-        case 'group':
-            zz = render_group(layer, leafer);
-            break;
-        case 'oval':
-            zz = render_oval(layer, leafer);
-            break;
-        case 'shapeGroup':
-            zz = render_shapeGroup(layer, leafer);
-            break;
-
-        default:
-            console.log(layer._class);
-            break;
-    }
-
-    if (zz === null) {
-        return null;
-    }
-
-    if (layer.layers && layer.layers) {
-        for (var x of layer.layers) {
-            const uu = renderLayer(x);
-            if (uu !== null) {
-                zz.add(uu);
-            }
-        }
-    }
-
-    return zz;
-}
-
-const render_text = (layer, leafer) => {
-
-    const { x, y, width, height } = layer.frame;
-
-    const t = new Text({
-        x,
-        y,
-        width,
-        height,
-        fill: '#000',
-        text: layer.attributedString.string,
-        fontSize: layer.attributedString.attributes[0].attributes.MSAttributedStringFontAttribute.attributes.size,
-        textAlign: 'center',
-        draggable: true
-    })
-
-    return t;
-}
-
-const render_rectangle = (layer, leafer) => {
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
-
-const render_shapePath = (layer, leafer) => {
-    return null;
-}
-
-const render_symbolMaster = (layer, leafer) => {
-
-    const { x, y, width, height } = layer.frame;
-
-    const d = new Frame({
-        x,
-        y,
-        width,
-        height,
-    });
-
-    return d;
-}
-
-const render_symbolInstance = (layer, leafer) => {
-
-    console.log(layer._class)
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
-
-const render_bitmap = (layer, leafer) => {
-
-    console.log(layer._class)
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
-
-const render_group = (layer, leafer) => {
-
-    console.log(layer._class)
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
-
-const render_oval = (layer, leafer) => {
-
-    console.log(layer._class)
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
-
-const render_shapeGroup = (layer, leafer) => {
-
-    console.log(layer._class)
-
-    const { x, y, width, height } = layer.frame;
-
-    const rect = new Rect({
-        x,
-        y,
-        width,
-        height,
-        fill: 'rgba(0, 0, 0, 0.2)',
-        draggable: true
-    })
-
-    return rect;
-}
 
 
 
 const App = () => {
 
     const ref = createRef();
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
 
     useEffect(() => {
 
@@ -233,7 +42,255 @@ const App = () => {
         }
     }, [ref])
 
-    return <div ref={ref} style={{ display: 'inline-block', width: '1200px', height: '1000px', border: '1px solid #f00' }}></div>
+
+    const renderLayer = (layer, leafer) => {
+
+        let zz = null;
+
+        switch (layer._class) {
+            case 'text':
+                zz = render_text(layer, leafer);
+                break;
+            case 'rectangle':
+                zz = render_rectangle(layer, leafer);
+                break;
+            case 'shapePath':
+                zz = render_shapePath(layer, leafer);
+                break;
+            case 'symbolMaster':
+                zz = render_symbolMaster(layer, leafer);
+                break;
+            case 'symbolInstance':
+                zz = render_symbolInstance(layer, leafer);
+                break;
+            case 'bitmap':
+                zz = render_bitmap(layer, leafer);
+                break;
+            case 'group':
+                zz = render_group(layer, leafer);
+                break;
+            case 'oval':
+                zz = render_oval(layer, leafer);
+                break;
+            case 'shapeGroup':
+                zz = render_shapeGroup(layer, leafer);
+                break;
+
+            default:
+                console.log(layer._class);
+                break;
+        }
+
+        if (zz === null) {
+            return null;
+        }
+
+        if (layer.layers && layer.layers) {
+            for (var x of layer.layers) {
+                const uu = renderLayer(x);
+                if (uu !== null) {
+                    zz.add(uu);
+                }
+            }
+        }
+
+        return zz;
+    }
+
+    const render_text = (layer, leafer) => {
+
+        const { x, y, width, height } = layer.frame;
+
+        const t = new Text({
+            x,
+            y,
+            width,
+            height,
+            fill: '#000',
+            text: layer.attributedString.string,
+            fontSize: layer.attributedString.attributes[0].attributes.MSAttributedStringFontAttribute.attributes.size,
+            textAlign: 'center'
+        })
+
+        t.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return t;
+    }
+
+    const render_rectangle = (layer, leafer) => {
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    const render_shapePath = (layer, leafer) => {
+        return null;
+    }
+
+    const render_symbolMaster = (layer, leafer) => {
+
+        const { x, y, width, height } = layer.frame;
+
+        const d = new Frame({
+            x,
+            y,
+            width,
+            height,
+        });
+
+        return d;
+    }
+
+    const render_symbolInstance = (layer, leafer) => {
+
+        console.log(layer._class)
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    const render_bitmap = (layer, leafer) => {
+
+        console.log(layer._class)
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    const render_group = (layer, leafer) => {
+
+        console.log(layer._class)
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    const render_oval = (layer, leafer) => {
+
+        console.log(layer._class)
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    const render_shapeGroup = (layer, leafer) => {
+
+        console.log(layer._class)
+
+        const { x, y, width, height } = layer.frame;
+
+        const rect = new Rect({
+            x,
+            y,
+            width,
+            height,
+            fill: 'rgba(0, 0, 0, 0.2)'
+        })
+
+        rect.on(PointerEvent.TAP, () => {
+            setX(x);
+            setY(y);
+            setWidth(width)
+            setHeight(height);
+        })
+
+        return rect;
+    }
+
+    return <>
+        <div ref={ref} style={{ display: 'inline-block', width: '1200px', height: '1000px', border: '1px solid #f00' }}></div>
+        <div style={{ position: 'absolute', right: '0', top: '0', bottom: '0', width: '300px', borderLeft: '1px solid #ccc', background: '#eee' }}>
+            <div>x = {x}</div>
+            <div>y = {y}</div>
+            <div>width = {width}</div>
+            <div>height = {height}</div>
+        </div>
+    </>
 }
 
 createRoot(document.getElementById("root")).render(<App />);
